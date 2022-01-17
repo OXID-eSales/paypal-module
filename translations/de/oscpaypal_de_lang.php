4bbbb9f26179d01a594fbc65aedb0d84f3ab1e7a
hkreuter <heike.reuter@oxid-esales.com>
2022-01-17 09:10:07 +0100

    PSPAYPAL-517 Add translations

---
+++ translations/de/oscpaypal_de_lang.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+$aLang = [
+    'charset'                                     => 'UTF-8',
+    'OXPS_PAYPAL_SUBSCRIBE'                       => 'PayPal Abonnements',
+    'OXPS_PAYPAL_DESCRIPTION'                     => 'Zahlung bei %s',
+    'OXPS_PAYPAL_PAY'                             => 'PayPal',
+    'OXPS_PAYPAL_PAY_PROCESSED'                   => 'Ihre Zahlung wird von PayPal verarbeitet.',
+    'OXPS_PAYPAL_PAY_UNLINK'                      => 'aufheben',
+    'OXPS_PAYPAL_SUBSCRIPTION'                    => 'PayPal Abonnement',
+    'OXPS_PAYPAL_SUBSCRIPTION_SETUP_FEE'          => 'Einrichtungsgebühr',
+    'OXPS_PAYPAL_FREQUENCY_DAY'                   => 'Tag',
+    'OXPS_PAYPAL_FREQUENCY_WEEK'                  => 'Woche',
+    'OXPS_PAYPAL_FREQUENCY_SEMI_MONTH'            => 'Halbmonatlich',
+    'OXPS_PAYPAL_FREQUENCY_MONTH'                 => 'Monat',
+    'OXPS_PAYPAL_FREQUENCY_YEAR'                  => 'Jahr',
+    'OXPS_PAYPAL_ORDEREXECUTION_ERROR'            => 'Fehler im Bestellvorgang. Bitte versuchen Sie es später erneut oder wählen Sie eine andere Zahlungsart.',
+    'OXPS_PAYPAL_FOR'                             => 'Für',
+    'OXPS_PAYPAL_SUBSCRIPTION_NEEDSUSER'          => 'Diesen Artikel können Sie abonnieren, wenn Sie sich in unserem Shop als Kunde registriert und angemeldet haben.',
+    'OXPS_PAYPAL_SUBSCRIPTION_NOTE'               => 'Dieses Abo haben Sie bereits mindestens einmal abgeschlossen.',
+
+    'OXPS_PAYPAL_BILLING_PLAN_MAIN'                => 'Information zum Abrechnungsplan des Abonnements',
+    'OXPS_PAYPAL_TENURE_TYPE'                      => 'Abo-Zeitart',
+    'OXPS_PAYPAL_TENURE_TYPE_TRIAL'                => 'Probeabo',
+    'OXPS_PAYPAL_TENURE_TYPE_REGULAR'              => 'Normal',
+    'OXPS_PAYPAL_SEQUENCE'                         => 'Sequenz',
+    'OXPS_PAYPAL_CYCLES_COMPLETED'                 => 'Zyklen abgeschlossen',
+    'OXPS_PAYPAL_CURRENT_PRICING_SCHEME_VERSION'   => 'Version des Preisschemas',
+    'OXPS_PAYPAL_TOTAL_CYCLES'                     => 'Gesamtzyklen',
+    'OXPS_PAYPAL_CYCLES_REMAINING'                 => 'verbleibende Zyklen',
+
+    'OXPS_PAYPAL_SUBSCRITION_PART_ARTICLE_TITLE'   => '%s (Teillieferung Nr. %s von %s)',
+    'OXPS_PAYPAL_SUBSCRITION_PART_NOTE'            => 'Das ist eine Teillieferung eines Abos. Die Informationen zu diesem Abo finden Sie in der Hauptbestellung Nr.',
+    'OXPS_PAYPAL_SUBSCRIPTION_NEXT_BILLING_TIME'   => 'Nächste Abrechnung',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE'         => 'Abo kündigen',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_NOTE'    => 'Wenn Sie dieses Abo kündigen wollen, klicken Sie bitte folgenden Button. Wir werden uns umgehend mit Ihnen in Verbindung setzen und das Ende des Abos entsprechend des Vertrags einleiten.',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_SEND'    => 'Abo-Kündigungsanfrage gesendet',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_HEAD'    => 'Abo-Kündigungsanfrage',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_MAIL'    => 'Der <strong>Kunde %s (Kd.Nr: %s)</strong> möchte sein Abo aus der Bestellung <strong>%s</strong> stornieren. Bitte prüfen Sie das Abo und setzen sich mit dem Kunden anschließend wieder in Verbindung.',
+];
---
+++ translations/en/oscpaypal_en_lang.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+$aLang = [
+    'charset'                                      => 'UTF-8',
+    'OXPS_PAYPAL_SUBSCRIBE'                        => 'PayPal Subscriptions',
+    'OXPS_PAYPAL_DESCRIPTION'                      => 'Payment at %s',
+    'OXPS_PAYPAL_PAY'                              => 'PayPal',
+    'OXPS_PAYPAL_PAY_PROCESSED'                    => 'Your payment will be processed by PayPal Pay.',
+    'OXPS_PAYPAL_PAY_UNLINK'                       => 'unlink',
+    'OXPS_PAYPAL_SUBSCRIPTION'                     => 'PayPal Subscription',
+    'OXPS_PAYPAL_SUBSCRIPTION_SETUP_FEE'           => 'Setup Fee',
+    'OXPS_PAYPAL_FREQUENCY_DAY'                    => 'Day',
+    'OXPS_PAYPAL_FREQUENCY_WEEK'                   => 'Week',
+    'OXPS_PAYPAL_FREQUENCY_SEMI_MONTH'             => 'Semi Week',
+    'OXPS_PAYPAL_FREQUENCY_MONTH'                  => 'Month',
+    'OXPS_PAYPAL_FREQUENCY_YEAR'                   => 'Year',
+    'OXPS_PAYPAL_ORDEREXECUTION_ERROR'             => 'Order execution error. Please try later or choose different payment method.',
+    'OXPS_PAYPAL_FOR'                              => 'For',
+    'OXPS_PAYPAL_SUBSCRIPTION_NEEDSUSER'           => 'You can subscribe to this article if you have registered and logged in as a customer in our shop.',
+    'OXPS_PAYPAL_SUBSCRIPTION_NOTE'                => 'You have already taken out this subscription at least once.',
+
+    'OXPS_PAYPAL_BILLING_PLAN_MAIN'                => 'Information about the subscription`s billingplans',
+    'OXPS_PAYPAL_TENURE_TYPE'                      => 'Tenure type',
+    'OXPS_PAYPAL_TENURE_TYPE_TRIAL'                => 'Trial',
+    'OXPS_PAYPAL_TENURE_TYPE_REGULAR'              => 'Regular',
+    'OXPS_PAYPAL_SEQUENCE'                         => 'Sequence',
+    'OXPS_PAYPAL_CYCLES_COMPLETED'                 => 'Cycles completed',
+    'OXPS_PAYPAL_CURRENT_PRICING_SCHEME_VERSION'   => 'Pricing scheme version',
+    'OXPS_PAYPAL_TOTAL_CYCLES'                     => 'Total cycles',
+    'OXPS_PAYPAL_CYCLES_REMAINING'                 => 'Cycles remaining',
+
+    'OXPS_PAYPAL_SUBSCRITION_PART_ARTICLE_TITLE'   => '%s (Partial delivery no. %s from %s)',
+    'OXPS_PAYPAL_SUBSCRITION_PART_NOTE'            => 'This is a partial delivery of a subscription. The information about this subscription can be found in the main order no.',
+    'OXPS_PAYPAL_SUBSCRIPTION_NEXT_BILLING_TIME'   => 'Next billing',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE'         => 'Subscriptions unsubscribe',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_NOTE'    => 'If you want to cancel this subscription, please click the following button. We will contact you immediately and initiate the end of the subscription according to the contract.',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_SEND'    => 'Subscription cancellation request sent',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_HEAD'    => 'Subscription cancellation request',
+    'OXPS_PAYPAL_SUBSCRIPTION_UNSUBSCRIBE_MAIL'    => 'The <strong> customer%s (Kd.Nr: %s)</strong> would like to cancel his subscription from the order <strong>%s</strong>. Please check the subscription and then contact the customer again.',
+];
