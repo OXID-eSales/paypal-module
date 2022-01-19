a053ba79d9e18e10e75b8091d536c2fe4d8f041b
hkreuter <heike.reuter@oxid-esales.com>
2022-01-19 18:15:53 +0100

    PSPAYPAL-517 Force module activation in tests

--- Tests/Codeception/Acceptance/BaseCest.php
+++ Tests/Codeception/Acceptance/BaseCest.php
@@ -10,12 +10,14 @@ declare(strict_types=1);
 namespace OxidSolutionCatalysts\PayPal\Tests\Codeception\Acceptance;
 
 use OxidSolutionCatalysts\PayPal\Tests\Codeception\AcceptanceTester;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Setup\Exception\ModuleSetupException;
 use OxidEsales\Facts\Facts;
 
 abstract class BaseCest
 {
     public function _before(AcceptanceTester $I): void
     {
+        $this->activateModules();
     }
 
     public function _after(AcceptanceTester $I): void
@@ -27,5 +29,26 @@ abstract class BaseCest
         $facts = new Facts();
 
         return $facts->getShopUrl();
+    }
+
+    /**
+     * Activates modules
+     */
+    protected function activateModules(int $shopId = 1): void
+    {
+        $testConfig        = new \OxidEsales\TestingLibrary\TestConfig();
+        $modulesToActivate = $testConfig->getModulesToActivate();
+
+        if ($modulesToActivate) {
+            $serviceCaller = new \OxidEsales\TestingLibrary\ServiceCaller();
+            $serviceCaller->setParameter('modulestoactivate', $modulesToActivate);
+
+            try {
+                $serviceCaller->callService('ModuleInstaller', $shopId);
+            } catch (ModuleSetupException $e) {
+                // this may happen if the module is already active,
+                // we can ignore this
+            }
+        }
     }
 }
