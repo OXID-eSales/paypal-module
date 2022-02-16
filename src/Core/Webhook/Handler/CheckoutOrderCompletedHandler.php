4347362dad922fdec74514f945cfbd9798061855
hkreuter <heike.reuter@oxid-esales.com>
2022-02-16 16:56:47 +0100

    Fix after merge

--- src/Core/Webhook/Handler/CheckoutOrderCompletedHandler.php
+++ src/Core/Webhook/Handler/CheckoutOrderCompletedHandler.php
@@ -14,12 +14,17 @@ use OxidSolutionCatalysts\PayPalApi\Model\Orders\Capture;
 use OxidSolutionCatalysts\PayPalApi\Model\Orders\Order;
 use OxidSolutionCatalysts\PayPalApi\Model\Orders\Order as OrderResponse;
 use OxidSolutionCatalysts\PayPalApi\Model\Orders\OrderCaptureRequest;
-use OxidSolutionCatalysts\PayPal\Core\ServiceFactory;
 use OxidSolutionCatalysts\PayPal\Core\Webhook\Event;
 use OxidSolutionCatalysts\PayPal\Exception\WebhookEventException;
+use OxidSolutionCatalysts\PayPal\Traits\ServiceContainer;
+use OxidSolutionCatalysts\PayPal\Service\OrderRepository;
+use OxidSolutionCatalysts\PayPal\Exception\NotFound;
+
 
 class CheckoutOrderCompletedHandler implements HandlerInterface
 {
+    use ServiceContainer;
+
     /**
      * @inheritDoc
      * @throws ApiException
@@ -27,11 +32,9 @@ class CheckoutOrderCompletedHandler implements HandlerInterface
     public function handle(Event $event): void
     {
         $data = $event->getData()['resource'];
+        $payPalOrderId = (string) $data['id'];
 
-        $payPalOrderId = $data['id'] ?? '';
-        $oxidOrderId = $data['purchase_units'][0]['custom_id'] ?? '';
-
-        if (!$oxidOrderId || !$payPalOrderId) {
+        if (!$payPalOrderId) {
             throw WebhookEventException::mandatoryDataNotFound();
         }
 
@@ -42,38 +45,14 @@ class CheckoutOrderCompletedHandler implements HandlerInterface
             $order = $orderRepository->getShopOrderByPayPalOrderId($payPalOrderId);
         } catch(NotFound $exception) {
             throw WebhookEventException::byPayPalOrderId($payPalOrderId);
-        $order = oxNew(EshopModelOrder::class);
-        if (!$order->load($oxidOrderId)) {
-            throw WebhookEventException::byOrderId($oxidOrderId);
         }
 
-        $response = $this->capturePayment($payPalOrderId);
-
+        //TODO: tbd: query order details from paypal. On the other hand, we just got verified that this data came from PayPal.
         if (
             $data['status'] == OrderResponse::STATUS_COMPLETED &&
             $data['purchase_units'][0]['payments']['captures'][0]['status'] == Capture::STATUS_COMPLETED
-            $response->status == OrderResponse::STATUS_COMPLETED &&
-            $response->purchase_units[0]->payments->captures[0]->status == Capture::STATUS_COMPLETED
         ) {
             $order->markOrderPaid();
         }
-    }
-
-    /**
-     * Captures payment for given order
-     *
-     * @param string $orderId
-     *
-     * @return Order
-     * @throws ApiException
-     */
-    private function capturePayment(string $orderId): Order
-    {
-        /** @var ServiceFactory $serviceFactory */
-        $serviceFactory = Registry::get(ServiceFactory::class);
-        $service = $serviceFactory->getOrderService();
-        $request = new OrderCaptureRequest();
-
-        return $service->capturePaymentForOrder('', $orderId, $request, '');
     }
 }
