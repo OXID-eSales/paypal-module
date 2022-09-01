870df629a10e5ff5955969ef80ca0b4c2ad8ac83
hkreuter <heike.reuter@oxid-esales.com>
2022-09-01 14:02:42 +0200

    PSPAYPAL-574 Update default value for oscpaypal_order.oscpaypaltransactionid

---
+++ migration/data/Version20220901134100.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\PayPal\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\Migrations\AbstractMigration;
+use OxidEsales\Eshop\Core\Config;
+use OxidEsales\Eshop\Core\ConfigFile;
+use OxidEsales\Facts\Facts;
+use OxidSolutionCatalysts\PayPal\Core\PayPalDefinitions;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220901134100 extends AbstractMigration
+{
+    public function __construct($version)
+    {
+        parent::__construct($version);
+
+        $this->platform->registerDoctrineTypeMapping('enum', 'string');
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->updatePayPalOrderTable($schema);
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+
+    /**
+     * update paypal order table
+     */
+    protected function updatePayPalOrderTable(Schema $schema): void
+    {
+        $order = $schema->getTable('oscpaypal_order');
+        if ($order->hasColumn('OSCPAYPALTRANSACTIONID')) {
+            $order->changeColumn(
+                'OSCPAYPALTRANSACTIONID',
+                [ 'notnull' => true,
+                  'default' => ''
+                ]
+            );
+        }
+    }
+}
