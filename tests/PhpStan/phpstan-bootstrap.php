d4eca185405b486c47061ddb8d38968302d3c12b
René Gust <info@rene-gust.de>
2023-05-22 19:16:37 +0200

    PSPAYPAL-594 some phpstan related improvements

--- tests/PhpStan/phpstan-bootstrap.php
+++ tests/PhpStan/phpstan-bootstrap.php
@@ -7,6 +7,10 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/../../../../../../vendor/autoload.php';
+
+define('TEST_LIBRARY_HELPERS_PATH', __DIR__ . '/../../../../../../vendor/oxid-esales/testing-library/library/helpers/');
+
 class_alias(
     OxidEsales\Eshop\Application\Component\UserComponent::class,
     OxidEsales\Eshop\Application\Component\UserComponent_parent::class
