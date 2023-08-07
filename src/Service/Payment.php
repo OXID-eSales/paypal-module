aefb7d2e91a88de033a56a96938c3881f953b5ab
Lars Stegelitz <lars.stegelitz@oxid-esales.com>
2023-08-07 20:26:52 +0200

    Fixed unknown class reference "Order" (imported as alias)

--- src/Service/Payment.php
+++ src/Service/Payment.php
@@ -315,7 +315,7 @@ class Payment
                 (string) $payPalTransactionId
             );
 
-            if ($result instanceof Order && $order->isPayPalOrderCompleted($result)) {
+            if ($result instanceof ApiOrderModel && $order->isPayPalOrderCompleted($result)) {
                 $order->setOrderNumber();
                 $order->markOrderPaid();
                 $order->setTransId((string) $payPalTransactionId);
