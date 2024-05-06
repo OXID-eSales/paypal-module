0ac5eae0d5da787bac8201826bc7d2ddf289282e
Danny <danny.zimmer@maexware-solutions.de>
2024-05-06 16:50:24 +0200

    PSPAYPAL-758: Add BasketComponent override to reset PayPal session on basket change or item addition

---
+++ src/Component/BasketComponent.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidSolutionCatalysts\PayPal\Component;
+
+use OxidEsales\Eshop\Core\Registry;
+use OxidSolutionCatalysts\PayPal\Core\PayPalSession;
+
+/**
+ * @mixin \OxidEsales\Eshop\Application\Component\BasketComponent
+ */
+class BasketComponent extends BasketComponent_parent
+{
+    /**
+     * Resets the session if the user has previously authorized a payment
+     * with PayPal Express and is now changing their basket.
+     *
+     * @param $sProductId
+     * @param $dAmount
+     * @param $aSel
+     * @param $aPersParam
+     * @param $blOverride
+     * @return mixed
+     */
+    public function changeBasket(
+        $sProductId = null,
+        $dAmount = null,
+        $aSel = null,
+        $aPersParam = null,
+        $blOverride = true
+    ) {
+
+        if ( PayPalSession::isPayPalExpressOrderActive()) {
+            PayPalSession::unsetPayPalOrderId();
+            Registry::getSession()->getBasket()->setPayment(null);
+        }
+        return parent::changeBasket($sProductId, $dAmount, $aSel, $aPersParam, $blOverride);
+    }
+
+    /**
+     * Resets the session if the user has previously authorized a payment
+     *  with PayPal Express and is now adding an item to the basket.
+     *
+     * @param $sProductId
+     * @param $dAmount
+     * @param $aSel
+     * @param $aPersParam
+     * @param $blOverride
+     * @return mixed
+     */
+    public function toBasket($sProductId = null, $dAmount = null, $aSel = null, $aPersParam = null, $blOverride = false)
+    {
+        if ( PayPalSession::isPayPalExpressOrderActive()) {
+            PayPalSession::unsetPayPalOrderId();
+            Registry::getSession()->getBasket()->setPayment(null);
+        }
+        return parent::toBasket($sProductId, $dAmount, $aSel, $aPersParam, $blOverride);
+    }
+}
