547f673fb6be153bc25ef0e36649754c9518072a
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-07-02 14:12:37 +0200

    move cert-URLs to Constants

--- src/Core/PayPalDefinitions.php
+++ src/Core/PayPalDefinitions.php
@@ -114,13 +114,7 @@ final class PayPalDefinitions
             'constraints' => self::PAYMENT_CONSTRAINTS_PAYPAL,
             'onlybrutto' => false,
             'buttonpayment' => false,
-            'defaulton' => true,
-            'sandbox' => [
-                'url' => 'https://paypalobjects.com/devdoc/apple-pay/sandbox/apple-developer-merchantid-domain-association',
-            ],
-            'live' => [
-                'url' => 'https://paypalobjects.com/devdoc/apple-pay/well-known/apple-developer-merchantid-domain-association',
-            ],
+            'defaulton' => true
         ],
         //Paylater PayPal
         self::PAYLATER_PAYPAL_PAYMENT_ID => [
