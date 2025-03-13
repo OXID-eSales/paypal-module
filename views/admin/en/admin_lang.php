d66dbb805709c94759becdf2d94fa1bc08b68c59
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-03-13 15:17:36 +0100

    Add stronger indication of required domain registration for Apple Pay

--- views/admin/en/admin_lang.php
+++ views/admin/en/admin_lang.php
@@ -5,6 +5,7 @@
  * See LICENSE file for license details.
  */
 
+use OxidSolutionCatalysts\PayPal\Core\Constants;
 use OxidSolutionCatalysts\PayPal\Core\PayPalDefinitions;
 
 $sLangName = 'English';
@@ -72,7 +73,7 @@ $aLang = [
     'OSC_PAYPAL_SPECIAL_PAYMENTS_PUI'             => 'Pay upon Invoice',
     'OSC_PAYPAL_SPECIAL_PAYMENTS_ACDC'            => 'Creditcard',
     'OSC_PAYPAL_SPECIAL_PAYMENTS_ACDC_FALLBACK'   => '<em>- Alternatively, you can activate and use the payment method "PayPal Credit Card Fallback" (id: "' . PayPalDefinitions::CCALTERNATIVE_PAYPAL_PAYMENT_ID . '"). Please use a suitable name for this payment method in this fallback case.</em>',
-
+    'OSC_PAYPAL_INSTALL_NOTE_APPLEPAY'            => '<em>- If you plan to use Apple Pay, you must finally add this shops domain to your PayPal account (Account Settings > Payment Sources > Apple Pay). Detailed instructions can be found in our module documentation under <a href="' . Constants::PAYPAL_DOCUMENTATION_EN . '" target="_blank">Configuration > Enable Apple Pay as a Payment Method</a>.</em>',
 
     'OSC_PAYPAL_SPECIAL_PAYMENTS_VAULTING'        => 'Vaulting',
     'OSC_PAYPAL_SPECIAL_PAYMENTS_APPLEPAY'        => 'Applepay',
