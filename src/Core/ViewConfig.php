aed7b0fe8efcdf9af0163ddaddb3e2cd5c7c7118
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2025-03-19 13:50:45 +0100

    PSPAYPAL-940 User sees PP Express on Details if PPE Session is active

--- src/Core/ViewConfig.php
+++ src/Core/ViewConfig.php
@@ -89,25 +89,22 @@ class ViewConfig extends ViewConfig_parent
         return $this->getServiceFromContainer(ModuleSettings::class)->isSandbox();
     }
 
-    /**
-     * @return bool
-     */
     public function isPayPalExpressSessionActive(): bool
     {
         return PayPalSession::isPayPalExpressOrderActive();
     }
 
-    /**
-     * @return bool
-     */
+    public function unsetPaypalSessionAndOrderId(): void
+    {
+        PayPalSession::unsetPayPalSession();
+        PaypalSession::unsetPayPalOrderId();
+    }
+
     public function isPayPalACDCSessionActive(): bool
     {
         return PayPalSession::isPayPalACDCOrderActive();
     }
 
-    /**
-     * @return bool
-     */
     public function isPayPalExpressPaymentEnabled(): bool
     {
         return $this->getServiceFromContainer(ModuleSettings::class)->isPayPalCheckoutExpressPaymentEnabled();
--- views/smarty/frontend/shared/page/details/inc/details_productmain_tobasket.tpl
+++ views/smarty/frontend/shared/page/details/inc/details_productmain_tobasket.tpl
@@ -1,4 +1,5 @@
 [{assign var="config" value=$oViewConf->getPayPalCheckoutConfig()}]
-[{if $blCanBuy && !$oDetailsProduct->isNotBuyable() && $config->isActive() && !$oViewConf->isPayPalExpressSessionActive() && $config->showPayPalProductDetailsButton()}]
+[{$oViewConf->unsetPaypalSessionAndOrderId()}]
+[{if $blCanBuy && !$oDetailsProduct->isNotBuyable() && $config->isActive() && $config->showPayPalProductDetailsButton()}]
     [{include file="@osc_paypal/frontend/shared/paymentbuttons.tpl" buttonId="PayPalButtonProductMain" buttonClass="paypal-button-wrapper large" aid=$oDetailsProduct->oxarticles__oxid->value}]
 [{/if}]
