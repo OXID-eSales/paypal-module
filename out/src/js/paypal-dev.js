60fe7bb833e259eeafb7ee33925da7e04a78d586
Bartosz Sosnowski <barteksosnowski711@gmail.com>
2025-03-28 06:36:07 +0100

    wip, js refactor

--- out/src/js/paypal-dev.js
+++ out/src/js/paypal-dev.js
@@ -1,15 +1,15 @@
 (function () {
 
-    let PayPalPaymentController = function () {
-        this.currentOrder = {
-            shop: null,
-            paypal: null
-        };
+    let PayPalPaymentController = function (config) {
+
+        this.config = config
 
         this.currentOrderDefaults = {
             shop: null,
             paypal: null
         };
+
+        this.currentOrder = null;
 
         this.reportError = function (message) {
             console.log(message);
@@ -17,12 +17,13 @@
         }
 
         this.resetCurrentOrder = function (response) {
-            this.currentOrder = this.currentOrderDefaults;
+            this.currentOrder = {...this.currentOrderDefaults};
         }
 
         this.setCreatePayPalOrderResponse = function (response) {
             if (null !== this.currentOrder.paypal) {
                 //some order is currently ...
+                //@TODO check if this part is needded. Its the case that pay button is clicked twice (maybe its impossibble)
                 debugger
             }
 
@@ -30,7 +31,7 @@
         }
 
         //@TODO this name is not accurate, change it
-        this.setCreateShopOrderResponse = function (response, orderType) {
+        this.setShopOrderData = function (response, orderType) {
             if (response.status === 'success') {
                 if (null !== this.currentOrder.shop) {
                     //some order is currently
@@ -38,6 +39,7 @@
                 }
 
                 this.currentOrder[orderType] = response
+                this.config.purchaseUnits.custom_id = response.shopOrderNumber;
             }
         }
 
@@ -55,6 +57,14 @@
             return this.currentOrder[orderType][name];
         }
 
+        this.getPurchaseUnits = function () {
+            return {
+                purchase_units: [
+                    {...this.config.purchaseUnits}
+                ]
+            }
+        }
+
         this.getCurrentOrderOxid = function () {
             return this.getCurrentOrderData('shopOrderId', 'shop');
         }
@@ -66,90 +76,96 @@
         this.getCurrentPayPalOrderId = function () {
             return this.getCurrentOrderData('orderID', 'paypal');
         }
+
+        this.getConfigValue = function (name) {
+            return undefined !== this.config[name] ? this.config[name] : null;
+        }
+
+        this.onShopOrderCreated = function (data) {
+            PayPalPayment.setShopOrderData(data.detail, 'shop');
+        }
+
+        this.init = function () {
+            this.resetCurrentOrder();
+
+            document.addEventListener('shopOrderCreated', this.onShopOrderCreated);
+
+            return this
+        }
+
+        this.createOrder = async function (data, actions) {
+            let result = await PayPalPayment.backendRequest('shopOrderCreationStatusUrl', {}, {
+                'deladrid': PayPalPayment.getConfigValue('deladrid')
+            });
+
+            document.dispatchEvent(new CustomEvent('shopOrderCreated', new Object({detail: {...result}})));
+
+            return actions.order.create(PayPalPayment.getPurchaseUnits());
+        }
+
+        this.patchOrder = async function (details) {
+            let result = await PayPalPayment.backendRequest('shopOrderPatchingStatusUrl', {}, {
+                'shopOrderId': PayPalPayment.getCurrentOrderOxid(),
+                'payPalOrderId': PayPalPayment.getCurrentPayPalOrderId()
+            });
+
+            if (result.status === 'success') {
+                window.location = PayPalPayment.getConfigValue('shopThankYouPageUrl');
+            }
+        }
+
+        this.captureOrder = async function (data, actions) {
+            PayPalPayment.setCreatePayPalOrderResponse(data);
+
+            return actions.order.capture().then(PayPalPayment.patchOrder);
+        }
+
+        this.cancelOrder = async function (data, actions) {
+            await PayPalPayment.backendRequest('shopOrderCancelStatusUrl', {}, {
+                'shopOrderId': PayPalPayment.getCurrentOrderOxid()
+            });
+
+            PayPalPayment.resetCurrentOrder();
+        }
+
+        this.handleError = function () {
+            window.location = PayPalPayment.getConfigValue('shopOrderErrorUrl');
+        }
+
+        this.backendRequest = async function (url, headers, body) {
+            let response = await fetch(PayPalPayment.getConfigValue(url), {
+                method: 'post',
+                headers: Object.assign({
+                    'content-type': 'application/json',
+                }, headers),
+                body: JSON.stringify(body)
+            });
+
+            const result = await response.json();
+
+            if (result.status !== 'success') {
+                PayPalPayment.handleError()
+            }
+
+            return result;
+        }
+
+        return this.init();
     }
 
-    window.PayPalPaymentController = new PayPalPaymentController();
+    let PayPalPayment = new PayPalPaymentController(new PayPalPaymentControllerConfigurator());
 
     window.addEventListener('PayPalSDKLoadedEvent', (event) => {
 
-        button = paypal.Buttons({
-            createOrder: async function (data, actions) {
-                let shopOrderCreationStatus = await fetch(window.PP_DATA_12321.shopOrderCreationStatusUrl, {
-                    method: 'post',
-                    headers: {
-                        'content-type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        'deladrid': window.PP_DATA_12321.deladrid
-                    })
-                });
-
-                let response = await shopOrderCreationStatus.json();
-                window.PayPalPaymentController.setCreateShopOrderResponse(response, 'shop');
-                window.PP_DATA_12321.purchaseUnits.custom_id = response.shopOrderNumber;
-                window.PP_DATA_12321.purchaseUnits.custom_id = window.PayPalPaymentController.getCurrentOrderNumber();
-
-                let purchaseUnits = {
-                    purchase_units: [
-                        {...window.PP_DATA_12321.purchaseUnits}
-                    ]
-                }
-
-                return actions.order.create(purchaseUnits);
-            },
-
-            onApprove: async function (data, actions) {
-                window.PayPalPaymentController.setCreatePayPalOrderResponse(data);
-
-                return actions.order.capture().then(async function (details) {
-                    let shopOrderPatchingStatus = await fetch(window.PP_DATA_12321.shopOrderPatchingStatusUrl, {
-                        method: 'post',
-                        headers: {
-                            'content-type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            'shopOrderId': window.PayPalPaymentController.getCurrentOrderOxid(),
-                            'payPalOrderId': window.PayPalPaymentController.getCurrentPayPalOrderId()
-                        })
-                    });
-
-                    let response = await shopOrderPatchingStatus.json();
-                    if (response.status === 'success') {
-                        window.location = window.PP_DATA_12321.shopThankYouPageUrl;
-                    }
-                });
-            },
-
-            onCancel: async function (data, actions) {
-                let shopOrderCancelStatus = await fetch(window.PP_DATA_12321.shopOrderCancelStatusUrl, {
-                    method: 'post',
-                    headers: {
-                        'content-type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        'shopOrderId': window.PayPalPaymentController.getCurrentOrderOxid()
-                    })
-                });
-
-                let response = await shopOrderCancelStatus.json();
-                window.PayPalPaymentController.resetCurrentOrder();
-
-                //@TODO remove if not used
-                if (response.status === 'success') {
-                }
-
-                if (response.status === 'error') {
-                    window.PayPalPaymentController.reportError('cancel error'); //@TODO improve error handling
-                }
-            },
-
-            onError: function (data) {
-                debugger
-                //fetch('[{$sSelfLink|cat:"cl=oscpaypalproxy&fnc=cancelPayPalPayment"}]');
-            }
+        let button = paypal.Buttons({
+            createOrder: PayPalPayment.createOrder,
+            onApprove: PayPalPayment.captureOrder,
+            onCancel: PayPalPayment.cancelOrder,
+            onError: PayPalPayment.handleError
         })
+
         if (button.isEligible()) {
-            button.render(window.PP_DATA_12321.buttonSelector);
+            button.render(PayPalPayment.getConfigValue('buttonSelector'));
         }
     });
 
