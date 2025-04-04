f6149231d5f0bb27d607710cb82b8161220bdd97
Bartosz Sosnowski <barteksosnowski711@gmail.com>
2025-04-04 16:06:55 +0200

    redundant parameter removed

--- src/Core/ViewConfig.php
+++ src/Core/ViewConfig.php
@@ -249,10 +249,6 @@ class ViewConfig extends ViewConfig_parent
 
         if ($this->getIsVaultingActive()) {
             $components[] = 'card-fields';
-
-            // check if this is correctly used parameter
-            // reference: https://developer.paypal.com/sdk/js/configuration/#vault
-            $params['vault'] = 'true';
         }
 
         if ($components) {
