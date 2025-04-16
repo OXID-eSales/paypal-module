281a40d98aacd6a60a7acb2fbcdf36fce7ef03c2
Michaël Parienti <michael.parienti@oxid-esales.com>
2025-04-16 13:46:22 +0200

    PSPAYPAL-722 Freeze products in backend for paypal order

--- metadata.php
+++ metadata.php
@@ -10,6 +10,7 @@ use OxidEsales\Eshop\Application\Component\UserComponent;
 use OxidEsales\Eshop\Application\Controller\OrderController;
 use OxidEsales\Eshop\Application\Controller\PaymentController;
 use OxidEsales\Eshop\Application\Controller\Admin\OrderMain;
+use OxidEsales\Eshop\Application\Controller\Admin\OrderArticle;
 use OxidEsales\Eshop\Application\Controller\Admin\OrderOverview;
 use OxidEsales\Eshop\Application\Model\Article;
 use OxidEsales\Eshop\Application\Model\Basket;
@@ -25,6 +26,7 @@ use OxidSolutionCatalysts\PayPal\Component\BasketComponent as PayPalBasketCompon
 use OxidSolutionCatalysts\PayPal\Component\UserComponent as PayPalUserComponent;
 use OxidSolutionCatalysts\PayPal\Controller\Admin\PayPalConfigController;
 use OxidSolutionCatalysts\PayPal\Controller\Admin\PayPalOrderController;
+use OxidSolutionCatalysts\PayPal\Controller\Admin\OrderArticle as PayPalOrderArticleController;
 use OxidSolutionCatalysts\PayPal\Controller\Admin\OrderMain as PayPalOrderMainController;
 use OxidSolutionCatalysts\PayPal\Controller\Admin\OrderOverview as PayPalOrderOverviewController;
 use OxidSolutionCatalysts\PayPal\Controller\OrderController as PayPalFrontEndOrderController;
@@ -80,6 +82,7 @@ $aModule = [
         UserComponent::class => PayPalUserComponent::class,
         BasketComponent::class => PayPalBasketComponent::class,
         OrderMain::class => PayPalOrderMainController::class,
+        OrderArticle::class => PayPalOrderArticleController::class,
         OrderOverview::class => PayPalOrderOverviewController::class,
         State::class => PayPalState::class
     ],
