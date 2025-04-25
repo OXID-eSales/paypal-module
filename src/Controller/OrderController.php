0de94e2151d00d2f96bbb81ce728530cdd14da9a
Bartosz Sosnowski <barteksosnowski711@gmail.com>
2025-04-25 16:26:50 +0200

    wip, googlepay works

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -307,13 +307,10 @@ class OrderController extends OrderController_parent
             $logger = $this->getServiceFromContainer(Logger::class);
             $logger->log('error', $exception->getMessage(), [$exception]);
         }
-        $result = [
-            'location' => [
-                'cl=order&fnc=finalizeGooglePay&token=' . $orderId
-            ]
-        ];
 
-        $this->outputJson($result);
+        $this->outputJson([
+            'token' => $orderId
+        ]);
     }
 
     public function captureAcdcOrder(): void
