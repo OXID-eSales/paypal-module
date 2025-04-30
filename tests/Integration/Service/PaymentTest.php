9590662674368f0d760f5774f0db9da775be91f6
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2025-04-30 10:46:29 +0200

    PSPAYPAL-862 Update experience context

--- tests/Integration/Service/PaymentTest.php
+++ tests/Integration/Service/PaymentTest.php
@@ -349,7 +349,7 @@ final class PaymentTest extends BaseTestCase
 
         $request->intent = OrderRequest::INTENT_CAPTURE;
         $request->purchase_units = $decoded['purchase_units'];
-        $request->application_context = $decoded['application_context'];
+        $request->experience_context = $decoded['experience_context'];
         $request->payment_source = $decoded['payment_source'];
         $request->processing_instruction = "ORDER_COMPLETE_ON_PAYMENT_APPROVAL";
 
