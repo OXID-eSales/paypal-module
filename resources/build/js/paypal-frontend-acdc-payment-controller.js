727eca3ad6b063f2402bfe772242d5d1353d0381
Bartosz Sosnowski <b.sosnowski@createit.com>
2025-05-23 12:48:19 +0200

    Order status ERROR after order cancels fix

--- resources/build/js/paypal-frontend-acdc-payment-controller.js
+++ resources/build/js/paypal-frontend-acdc-payment-controller.js
@@ -18,10 +18,12 @@
                 'deliveryAddressId': PayPalPayment.getConfigValue('deliveryAddressId'),
                 'vaultPayment': PayPalPayment.currentOrder.vaultPayment
             });
-
             if (undefined !== result.error) {
                 return false;
             }
+
+            document.dispatchEvent(new CustomEvent('shopOrderCreated', new Object({detail: {...result.shopOrder}})));
+            document.dispatchEvent(new CustomEvent('payPalOrderCreated', new Object({detail: {...result.payPalOrder}})));
 
             if (result.payPalOrder.status === 'PAYER_ACTION_REQUIRED' || result.payPalOrder.status === 'APPROVED' ){
                 for (const i in result.payPalOrder.links) {
