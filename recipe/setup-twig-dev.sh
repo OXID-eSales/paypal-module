2cb40dd504e3e6edc904cc58415721c50947c2a8
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2025-06-25 12:53:53 +0200

    update gha runner for 8.0.x

--- recipe/setup-twig-dev.sh
+++ recipe/setup-twig-dev.sh
@@ -45,16 +45,16 @@ make file=services/adminer.yml addservice
 echo "module root is $MODULE_ROOT"
 cd "$PROJECT_ROOT" || exit 1
 
-$MODULE_ROOT/recipe/parts/b-7.3.x/start_shop.sh -e"${edition}" -u"false" || exit 1
+$MODULE_ROOT/recipe/parts/shared/prepare_shop_package.sh -e"${edition}" -u"false" -b"b-8.0.x" || exit 1
 
 mkdir -p "$PROJECT_ROOT"/source/extensions || exit 1
 cp -r "$MODULE_ROOT" "$PROJECT_ROOT"/source/extensions/ || exit 1
-git clone git@github.com:OXID-eSales/paypal-client.git --branch=v3.0.17 "$PROJECT_ROOT"/source/extensions/paypal-client || exit 1
+# git clone git@github.com:OXID-eSales/paypal-client.git --branch=v3.0.17 "$PROJECT_ROOT"/source/extensions/paypal-client || exit 1
 mkdir -p ./source/var/configuration/environment/shops/1/modules
 cp $MODULE_ROOT/recipe/environment/1.yaml ./source/var/configuration/environment/shops/1/modules/osc_paypal.yaml
 
-
-$PROJECT_ROOT/source/extensions/paypal/recipe/parts/b-8.0.x/require_twig_components.sh -e"${edition}" -t"apex" || exit 1
+$PROJECT_ROOT/source/extensions/paypal/recipe/parts/shared/require_twig_components.sh -e"${edition}" -b"b-8.0.x" || exit 1
+$PROJECT_ROOT/source/extensions/paypal/recipe/parts/shared/require_theme_dev.sh -t"apex" -b"b-8.0.x" || exit 1
 
 # Require demodata package
 docker compose exec -T \
@@ -64,8 +64,7 @@ docker compose exec -T php composer require oxid-esales/oxideshop-demodata-ee:de
 
 # Install all preconfigured dependencies
 docker compose exec -T php composer update --no-interaction
-
-$PROJECT_ROOT/source/extensions/paypal/recipe/parts/shared/setup_database.sh
+docker compose exec php bin/oe-console oe:database:reset --force
 
 perl -pi\
   -e 'print "SetEnvIf Authorization \"(.*)\" HTTP_AUTHORIZATION=\$1\n\n" if $. == 1'\
@@ -106,6 +105,6 @@ echo -e "\033[1;37m\033[1;42mYou can now access your shop at http://localhost.lo
 echo -e "\033[1;37m\033[1;42mShop admin at http://localhost.local/admin\033[0m\n"
 echo -e "\033[1;37m\033[1;42mYou can access the Adminer at http://localhost.local:8080/\033[0m\n"
 
-rm -rf "$MODULE_ROOT"
+# rm -rf "$MODULE_ROOT"
 
 exit 0
