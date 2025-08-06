061ba9eed628e290c6fa8e8e4f09bb6f39239035
razvan.zerfas@betterqa.co <razvan.zerfas@betterqa.co>
2025-08-06 15:39:43 +0300

    My first commit

--- tests/e2e/playwright/helpers/ShopHelper.js
+++ tests/e2e/playwright/helpers/ShopHelper.js
@@ -151,7 +151,8 @@ export class ShopHelper {
         const paypalIframeLocator = await page.frameLocator('.component-frame.visible').nth(0);
         console.log('Targeting refined PayPal iframe selector.');
 
-        const paypalButtonLocator = paypalIframeLocator.locator('div.paypal-button');
+        // const paypalButtonLocator = paypalIframeLocator.locator('div.paypal-button');
+        const paypalButtonLocator = await paypalIframeLocator.locator('[data-funding-source="paypal"]');
 
         console.log('PayPal button is visible. Clicking...');
         await paypalButtonLocator.click();
