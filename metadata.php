4ed6f808a7a19b6eb035633c0d0227db654e387a
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-08-14 16:12:26 +0200

    Version

--- metadata.php
+++ metadata.php
@@ -68,7 +68,7 @@ $aModule = [
         'en' => 'Use of the online payment service from PayPal. Documentation: <a href="https://docs.oxid-esales.com/modules/paypal-checkout/en/latest/" target="_blank">PayPal Checkout</a>'
     ],
     'thumbnail' => 'img/paypal.png',
-    'version' => '3.5.0-rc.9',
+    'version' => '3.5.0-rc.10',
     'author' => 'OXID eSales AG',
     'url' => 'https://www.oxid-esales.com',
     'email' => 'info@oxid-esales.com',
