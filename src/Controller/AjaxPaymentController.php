306591ad52d7805f1ce22159f3b4010eeb83fc6e
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-09-30 16:55:05 +0200

    fix wrong dispatcher-calls

--- src/Controller/AjaxPaymentController.php
+++ src/Controller/AjaxPaymentController.php
@@ -103,7 +103,7 @@ class AjaxPaymentController extends ProxyController
         ];
 
         try {
-            $payPalOrder = new \OxidSolutionCatalysts\PayPalApi\Model\Orders\Order(PayPalSession::getCheckoutOrder());
+            $payPalOrder = new PayPalApiOrder(PayPalSession::getCheckoutOrder());
 
             //Verify 3D result if acdc payment
             if (!$paymentService->verify3D($paymentId, $payPalOrder)) {
@@ -169,7 +169,7 @@ class AjaxPaymentController extends ProxyController
                 $transactionId,
                 $payPalCustomerId
             );
-            $this->dispatcher->dispatch(PayPalOrderCompletedEvent::NAME, $event);
+            $this->dispatcher->dispatch($event, PayPalOrderCompletedEvent::NAME);
 
             if ($capturePaymentForOrder) {
                 $response['paymentStatus'] = $capturePaymentForOrder->getCapturePaymentStatus() ? 'success' : 'error';
