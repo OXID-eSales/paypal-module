169ecc79a6d0d2b7e2ef168594bb8ee4fa92063c
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-10-09 16:16:40 +0200

    setLogger again ...

--- tests/Integration/Model/OrderTest.php
+++ tests/Integration/Model/OrderTest.php
@@ -166,6 +166,7 @@ final class OrderTest extends BaseTestCase
                 'isPayPalOrderCompleted',
                 'isOrderFinished',
                 'isOrderPaid',
+                'setLogger',
                 'isWaitForWebhookTimeoutReached',
                 'load'
             ])
@@ -196,6 +197,7 @@ final class OrderTest extends BaseTestCase
 
         $orderMock = $this->patchMock($orderMock);
         $orderMock->setPaymentService($paymentServiceMock);
+        $orderMock->setLogger($this->createMock(LoggerInterface::class));
         $session = EshopRegistry::getSession();
         $session->setVariable('sess_challenge', 'test_challenge');
         EshopRegistry::set(Session::class, $session);
