737e41e64f027e90f21eb062457e96686ccef38c
Bartosz Sosnowski <barteksosnowski711@gmail.com>
2025-10-21 17:52:04 +0200

    Error handling fix

--- resources/build/js/paypal-frontend-standard-payment-controller.js
+++ resources/build/js/paypal-frontend-standard-payment-controller.js
@@ -49,6 +49,9 @@
             PayPalPayment.removeErrorMessage();
             PayPalPayment.addSubmitButtonOverlay();
             PayPalPayment.paypalOverlayWatcher();
+
+            document.dispatchEvent(new CustomEvent('beforeShopOrderCreated'));
+
             let checkTermsAndConditions = PayPalPayment.checkTermsAndConditions();
 
             if(false === checkTermsAndConditions) {
