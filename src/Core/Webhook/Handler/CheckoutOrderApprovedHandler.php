0cd8536fb62dafcb30dd4b82994a2791245435c6
Mario Lorenz <mario.lorenz@oxid-esales.com>
2026-02-17 08:03:56 +0100

    fix wrong parameter list.

--- src/Core/Webhook/Handler/CheckoutOrderApprovedHandler.php
+++ src/Core/Webhook/Handler/CheckoutOrderApprovedHandler.php
@@ -25,7 +25,7 @@ class CheckoutOrderApprovedHandler extends WebhookHandlerBase
         array $eventPayload,
         EshopModelOrder $order
     ): void {
-        $this->handleWebhookDelay($order, $payPalOrderId);
+        $this->handleWebhookDelay($payPalOrderId, $eventPayload, $order);
 
         if ($this->needsCapture($eventPayload)) {
             try {
