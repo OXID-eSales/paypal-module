ac6d6ee67cb24eda077743625fc383c0c4e9e5b7
Mario Lorenz <mario.lorenz@oxid-esales.com>
2026-02-17 17:31:05 +0100

    other check strategy

--- src/Controller/ProxyController.php
+++ src/Controller/ProxyController.php
@@ -373,16 +373,16 @@ class ProxyController extends FrontendController
      */
     private function cancelPendingPayPalOrder(): void
     {
+        $session = Registry::getSession();
+        $shopOrderId = (string)$session->getVariable('sess_challenge');
+
         $paymentService = $this->getServiceFromContainer(PaymentService::class);
 
-        if (!$paymentService->isOrderExecutionInProgress()) {
+        if (!$paymentService->getTemporaryOrder()) {
             return;
         }
 
-        $session = Registry::getSession();
-        $shopOrderId = (string)$session->getVariable('sess_challenge');
         $payPalOrderId = PayPalSession::getCheckoutOrderId();
-
 
         /** @var LoggerInterface $logger */
         $logger = $this->getServiceFromContainer('OxidSolutionCatalysts\PayPal\Logger');
