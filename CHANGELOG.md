93921f1ad263a281678ef1fceaa81e4452424312
Mario Lorenz <mario.lorenz@oxid-esales.com>
2026-02-19 09:23:54 +0100

    release v3.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [3.6.0] - 2026-02-17
+## [3.6.0] - 2026-02-19
 
 ### NEW
 
--- metadata.php
+++ metadata.php
@@ -68,7 +68,7 @@ $aModule = [
         'en' => 'Use of the online payment service from PayPal. Documentation: <a href="https://docs.oxid-esales.com/modules/paypal-checkout/en/latest/" target="_blank">PayPal Checkout</a>'
     ],
     'thumbnail' => 'img/paypal.png',
-    'version' => '3.6.0-rc.5',
+    'version' => '3.6.0',
     'author' => 'OXID eSales AG',
     'url' => 'https://www.oxid-esales.com',
     'email' => 'info@oxid-esales.com',
