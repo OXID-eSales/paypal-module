04fda17bd7493860c2ac4b9b150421cd29a095ef
Mario Lorenz <mario.lorenz@oxid-esales.com>
2026-02-27 08:48:59 +0100

    use cancelPayPalOrder also in Webhooks

--- src/Service/OrderRepository.php
+++ src/Service/OrderRepository.php
@@ -173,8 +173,7 @@ class OrderRepository
         foreach ($ids as $id) {
             $order = oxNew(EshopModelOrder::class);
             if ($order->load($id['oxid'])) {
-                // storno
-                $order->cancelOrder();
+                $order->cancelPayPalOrder();
             }
         }
     }
